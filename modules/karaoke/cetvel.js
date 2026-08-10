72d17745a833bf0990643535eda8eb4895013cf2
princessmayosko <princessmayosko@gmail.com>
2026-08-10 20:30:21 +0300

    Update cetvel.js

--- modules/karaoke/cetvel.js
+++ modules/karaoke/cetvel.js
@@ -1,34 +1,67 @@
-console.log("CETVEL YENİ");
+console.log("CETVEL JS ÇALIŞTI");
 
 
-
-const cetvel =
-document.getElementById("cetvel");
-
-
-const tab =
-document.getElementById("cetvel-tab");
+const cetvel = document.getElementById("cetvel");
+const tab = document.getElementById("cetvel-tab");
 
 
+if(tab && cetvel){
 
-if(tab){
+    tab.onclick=function(){
 
-tab.onclick=function(){
+        cetvel.classList.toggle("open");
 
-cetvel.classList.toggle("open");
+        console.log("cetvel tıklandı");
+
+    };
 
 }
-
-}
-
-
 
 
 
 function updateCetvel(note){
 
+    console.log("Gelen nota:",note);
 
-console.log(note);
 
+    document.querySelectorAll(".pitch-note")
+    .forEach(el=>{
+
+        el.classList.remove("active");
+
+    });
+
+
+
+    let notes = {
+
+        "do":"DO",
+        "si":"Sİ",
+        "la":"LA",
+        "sol":"SOL",
+        "fa":"FA",
+        "mi":"Mİ",
+        "re":"RE"
+
+    };
+
+
+    let target = notes[note];
+
+
+    if(target){
+
+        document.querySelectorAll(".pitch-note")
+        .forEach(el=>{
+
+            if(el.innerText.trim()==target){
+
+                el.classList.add("active");
+
+            }
+
+        });
+
+    }
 
 }
