2e82db30dd4233ba45bd06b0e0054b7355b60be7
princessmayosko <princessmayosko@gmail.com>
2026-08-10 19:07:34 +0300

    Update pitch-checker.js

--- modules/karaoke/pitch-checker.js
+++ modules/karaoke/pitch-checker.js
@@ -1,1 +1,66 @@
+console.log("PITCH CHECKER ÇALIŞTI");
 
+
+let currentTargetNotes = [];
+
+
+// Aktif kelimenin notalarını al
+
+function setTargetNotes(noteString){
+
+    if(!noteString){
+
+        currentTargetNotes=[];
+
+        return;
+
+    }
+
+
+    currentTargetNotes =
+    noteString
+    .split(" ")
+    .map(n=>normalizeNote(n));
+
+
+}
+
+
+
+
+// Oktav kaldırma
+
+function normalizeNote(note){
+
+    return note
+    .replace(/[0-9]/g,"")
+    .toLowerCase();
+
+}
+
+
+
+
+
+// Gelen notayı kontrol et
+
+function checkPitch(note){
+
+
+    if(!currentTargetNotes.length){
+
+        return false;
+
+    }
+
+
+
+    let clean =
+    normalizeNote(note);
+
+
+
+    return currentTargetNotes.includes(clean);
+
+
+}
